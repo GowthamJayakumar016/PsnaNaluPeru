21b815d0cf8fe0f03cab6d8109f29d9f6945fa6b
Aravind <aravindruthra17@gmail.com>
2026-03-29 12:35:03 +0530

    created by aravind

---
+++ DTOs/Hotel/HotelViewDto.cs
@@ -0,0 +1,13 @@
+namespace Happy.DTOs.Hotel
+{
+    public class HotelViewDto
+    {
+        public int Id { get; set; }
+    public string Name { get; set; }
+
+        public string Location { get; set; }
+
+        public string Address { get; set; }
+    }
+
+}
